499dbe3cb12e7778cf0fe4f10668e424bb1cc281
KID <jyhsu2000@yahoo.com.tw>
2019-11-07 20:18:35 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int addNumber(int a, int b) {
 }
 
 int getTriangleType(int a, int b, int c) {
-    // 註解註解123
+    // 註解註解asd
     if (a > b && a > c) {
         int temp = c; c = a; a = temp;
     }
